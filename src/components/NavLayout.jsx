b4e6fe099984efd621157ebc9f000877772058c7
Kristiana <kristianausacka@gmail.com>
2024-04-22 22:24:38 +0200

    Add page design

--- src/components/NavLayout.jsx
+++ src/components/NavLayout.jsx
@@ -5,10 +5,10 @@ export default function Layout() {
         <div>
             <nav className="bg-teal-900">
                 <div className="flex flex-row justify-center py-10 gap-60">   
-                    <Link to="/" className="py-7 px-10 min-w-32 border-solid border-2 border-green-950 rounded text-center">
+                    <Link to="/" className="py-7 px-10 min-w-32 border-solid border-2 border-green-950 rounded text-center bg-teal-950 hover:bg-zinc-600">
                         Home
                     </Link>  
-                    <Link to="/add" className="py-7 px-10 min-w-32 border-solid border-2 border-green-950 rounded text-center">  
+                    <Link to="/add" className="py-7 px-10 min-w-32 border-solid border-2 border-green-950 rounded text-center bg-teal-950 hover:bg-zinc-600">  
                         Add
                     </Link>      
                 </div>
--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -2,20 +2,18 @@ import { Outlet, Link } from "react-router-dom";
 
 export default function AddPage() {
     return (
-     <>
-        <ul>
-            <li>
-                <Link to="/add/expenses">Expenses</Link>
-            </li>
-            <li>
-                <Link to="/add/income">Income</Link>    
-            </li>                    
-            <li>
-                <Link to="/add/savings">Savings</Link>    
-            </li>                    
-        </ul>
-        <Outlet />
-     </>
+     <div className="flex flex-row">
+        <div className="flex flex-col mt-20 ml-48 gap-32">
+            <Link to="/add/expenses" className="py-4 px-9 min-w-32 border-solid border-2 border-green-950 rounded text-center bg-teal-950 hover:bg-zinc-600">Expenses</Link>
+        
+            <Link to="/add/income" className="py-4 px-9 min-w-32 border-solid border-2 border-green-950 rounded text-center bg-teal-950 hover:bg-zinc-600">Income</Link>    
+        
+            <Link to="/add/savings" className="py-4 px-9 min-w-32 border-solid border-2 border-green-950 rounded text-center bg-teal-950 hover:bg-zinc-600">Savings</Link>    
+        </div>
+        <div className="mt-5 mx-20">
+            <Outlet />
+        </div>
+     </div>
                
     );
 };
