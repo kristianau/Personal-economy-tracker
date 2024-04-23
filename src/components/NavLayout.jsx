17e6dc5b03d65506837dfe28224365ebcc7e7865
Kristiana <kristianausacka@gmail.com>
2024-04-23 15:36:55 +0200

    Navigation bar and add page navigation design creation

--- src/components/NavLayout.jsx
+++ src/components/NavLayout.jsx
@@ -2,14 +2,14 @@ import { Outlet, Link } from "react-router-dom";
 
 export default function Layout() {
     return (
-        <div>
-            <nav className="bg-teal-900">
+        <div className="h-screen flex flex-col">
+            <nav className="bg-gray-400">
                 <div className="flex flex-row justify-center py-10 gap-60">   
-                    <Link to="/" className="py-7 px-10 min-w-32 border-solid border-2 border-green-950 rounded text-center bg-teal-950 hover:bg-zinc-600">
-                        Home
+                    <Link to="/" className="py-7 px-10 min-w-32 border-solid border border-cyan-700 rounded text-center font-semibold tracking-widest bg-cyan-600 hover:bg-cyan-500">
+                        HOME
                     </Link>  
-                    <Link to="/add" className="py-7 px-10 min-w-32 border-solid border-2 border-green-950 rounded text-center bg-teal-950 hover:bg-zinc-600">  
-                        Add
+                    <Link to="/add" className="py-7 px-10 min-w-32 border-solid border border-cyan-700 rounded text-center font-semibold tracking-widest bg-cyan-600 hover:bg-cyan-500">  
+                        ADD
                     </Link>      
                 </div>
             </nav>
--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -2,13 +2,13 @@ import { Outlet, Link } from "react-router-dom";
 
 export default function AddPage() {
     return (
-     <div className="flex flex-row">
-        <div className="flex flex-col mt-20 ml-48 gap-32">
-            <Link to="/add/expenses" className="py-4 px-9 min-w-32 border-solid border-2 border-green-950 rounded text-center bg-teal-950 hover:bg-zinc-600">Expenses</Link>
+     <div className="flex flex-row flex-1">
+        <div className="flex flex-col pt-32 pl-80 pr-32 gap-32 bg-gray-300">
+            <Link to="/add/expenses" className="py-4 px-9 min-w-32 border-solid border border-cyan-700 rounded text-center font-medium tracking-wider bg-cyan-600 hover:bg-cyan-500">Expenses</Link>
         
-            <Link to="/add/income" className="py-4 px-9 min-w-32 border-solid border-2 border-green-950 rounded text-center bg-teal-950 hover:bg-zinc-600">Income</Link>    
+            <Link to="/add/income" className="py-4 px-9 min-w-32 border-solid border border-cyan-700 rounded text-center font-medium tracking-wider bg-cyan-600 hover:bg-cyan-500">Income</Link>    
         
-            <Link to="/add/savings" className="py-4 px-9 min-w-32 border-solid border-2 border-green-950 rounded text-center bg-teal-950 hover:bg-zinc-600">Savings</Link>    
+            <Link to="/add/savings" className="py-4 px-9 min-w-32 border-solid border border-cyan-700 rounded text-center font-medium tracking-wider bg-cyan-600 hover:bg-cyan-500">Savings</Link>    
         </div>
         <div className="mt-5 mx-20">
             <Outlet />
