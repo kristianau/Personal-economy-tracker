10e30d346c0dc4013e8badf3a073a25c3f7f7bba
Kristiana <kristianausacka@gmail.com>
2024-05-11 13:26:03 +0200

    Display SavingsBuckets items in a list in the modal window

--- src/components/BaseElements/EditSavingsModal.jsx
+++ src/components/BaseElements/EditSavingsModal.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import { savingsBuckets } from '../../utils/SavingsOptions'
 
 export default function Modal({ setModal }) {
+
+    const categoryList = savingsBuckets.map(category =>
+        <li>{category.label}</li>
+    );
+
     return (
         <div className='w-screen h-screen bg-black bg-opacity-30 fixed top-0 right-0 flex justify-center items-center'>
-            <div className='bg-white p-10 rounded-md shadow-md'>
-                <h1 className='font-bold text-center text-lg my-5'>Edit savings categories</h1>
-                <div className='flex justify-between mt-5'>
-                    <button className='outline outline-1 outline-[#101f20] bg-[#101f20] text-white py-2 px-4 hover:bg-transparent hover:text-black'
-                        onClick={() => setModal(false)}
-                    >Exit</button>
-                </div>
+            <div className='bg-white pt-2 p-10 rounded-md shadow-md'>
+                <h1 className='font-bold text-center text-lg'>Edit savings categories</h1>
+                <ul>{categoryList}</ul>
+                <button className='outline outline-1 outline-[#101f20] bg-[#101f20] text-white py-2 px-4 hover:bg-transparent hover:text-black'
+                    onClick={() => setModal(false)}
+                >Exit</button>
+
             </div>
         </div>
     )
