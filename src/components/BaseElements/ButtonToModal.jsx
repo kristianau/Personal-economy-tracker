1f4ca3111b5118e0f8acb05f88e172f0e3a57e62
Kristiana <kristianausacka@gmail.com>
2024-05-11 13:13:14 +0200

    Move Edit savings categories button's code to a new file

---
+++ src/components/BaseElements/ButtonToModal.jsx
@@ -0,0 +1,16 @@
+import Modal from "./EditSavingsModal";
+import { useState } from "react";
+
+export default function ModalButton() {
+
+    const [modal, setModal] = useState(false);
+
+    return (
+        <>
+            <button className="w-24 font-medium rounded border-solid border py-1 px-6 border-slate-300"
+                onClick={() => setModal(true)}
+            >Edit</button>
+            {modal && <Modal setModal={setModal} />}
+        </>
+    )
+}
--- src/components/InputElements/SavingsBucketInput.jsx
+++ src/components/InputElements/SavingsBucketInput.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import SavingsBucketDropdown from "../BaseElements/SavingsBucketDropdown";
 import { Controller, useFormContext } from "react-hook-form";
 import ErrorMessage from "../Messages/ErrorMessage";
-import Modal from "../BaseElements/EditSavingsModal";
+import ModalButton from "../BaseElements/ButtonToModal";
 
 export default function SavingsBucketInput({ label, id, placeholder }) {
 
     const { control } = useFormContext();
-    const [modal, setModal] = useState(false);
 
     return (
         <div className="flex flex-col w-full gap-2">
@@ -29,10 +28,7 @@ export default function SavingsBucketInput({ label, id, placeholder }) {
                         </div>
                         <div className="flex flex-row gap-2 items-center">
                             <SavingsBucketDropdown placeholder={placeholder} field={field} id={id} />
-                            <button className="w-24 font-medium rounded border-solid border py-1 px-5 border-slate-300"
-                                onClick={() => setModal(true)}
-                            >Edit</button>
-                            {modal && <Modal setModal={setModal} />}
+                            <ModalButton />
                         </div>
                     </div >
                 )
