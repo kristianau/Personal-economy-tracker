96f947438cc69e824e688f154376cabfd75341f2
Kristiana <kristianausacka@gmail.com>
2024-05-13 19:25:58 +0200

    Remove a redundant item from savingsBuckets list

--- src/utils/SavingsOptions.jsx
+++ src/utils/SavingsOptions.jsx
@@ -3,5 +3,4 @@ export const savingsBuckets = [
     { id: 2, value: "norwegianCourse", label: "Norwegian Course" },
     { id: 3, value: "newPhone", label: "New phone" },
     { id: 4, value: "gifts", label: "Gifts" },
-    { id: 5, value: "ipad", label: "Ipad" },
 ];
