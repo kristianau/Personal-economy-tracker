53fe9508d405668ca45c2d9f9de43f0b51bc32c8
Kristiana <kristianausacka@gmail.com>
2024-05-13 19:12:31 +0200

    Added more objects to savingsBuckets list.

--- src/utils/SavingsOptions.jsx
+++ src/utils/SavingsOptions.jsx
@@ -1,4 +1,7 @@
 export const savingsBuckets = [
     { id: 1, value: "ipad", label: "Ipad" },
-    { id: 2, value: "norwegianCourse", label: "Norwegian Course" }
+    { id: 2, value: "norwegianCourse", label: "Norwegian Course" },
+    { id: 3, value: "newPhone", label: "New phone" },
+    { id: 4, value: "gifts", label: "Gifts" },
+    { id: 5, value: "ipad", label: "Ipad" },
 ];
