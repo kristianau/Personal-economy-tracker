726b052f43b47b2e0c852a46633c51de8b6def4e
Kristiana <kristianausacka@gmail.com>
2024-05-14 23:02:20 +0200

    Updated <Select> tag value to an array if the user is allowed to select multiple options.

--- src/components/BaseElements/Dropdown.jsx
+++ src/components/BaseElements/Dropdown.jsx
@@ -11,11 +11,14 @@ export default function Dropdown({ placeholder, field, id }) {
         options = expensesOptions;
     }
 
+    // Ensure that field.value is always an array when multiple is true
+    const selectedValue = multiple ? (Array.isArray(field.value) ? field.value : []) : field.value;
+
     return (
         <Select
             id={id}
             onChange={field.onChange}
-            value={field.value}
+            defaultValue={selectedValue}
             multiple={multiple}
             className="font-medium w-full"
             name="currencySelect"
