d589eac1e8508cfed7cafc0b071bb64651c8ee51
Kristiana <kristianausacka@gmail.com>
2024-05-25 21:25:15 +0200

    blaljfdgdg

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -2,13 +2,13 @@ import { Link, Outlet } from "react-router-dom";
 
 export default function AddPage() {
     return (
-        <div className="flex flex-row flex-1">
-            <div className="flex flex-col gap-10 bg-white items-end content-center">
-                <Link to="/add/income" className="py-3 w-20 border-solid border border-black text-center font-medium tracking-wider">Income</Link>
+        <div className="flex flex-row">
+            <div className="flex flex-col pr-16 pl-28 pt-28 gap-16 bg-white content-center align-middle">
+                <Link to="/add/income" className="py-3 px-16 min-w-32 border-solid border border-black text-center font-medium tracking-wider">Income</Link>
 
-                <Link to="/add/savings" className="py-3 w-1/5 border-solid border border-black text-center font-medium tracking-wider">Savings</Link>
+                <Link to="/add/savings" className="py-3 px-16 min-w-32 border-solid border border-black text-center font-medium tracking-wider">Savings</Link>
 
-                <Link to="/add/expenses" className="py-3 w-1/5 border-solid border border-black text-center font-medium tracking-wider">Expenses</Link>
+                <Link to="/add/expenses" className="py-3 px-16 min-w-32 border-solid border border-black text-center font-medium tracking-wider">Expenses</Link>
             </div>
             <Outlet className="mt-3 mx-4"></Outlet>
         </div>
