e6e6797fd40ac98c2717d6fda7ad37e79e10906c
Kristiana <kristianausacka@gmail.com>
2024-05-26 21:14:53 +0200

    Authentication page design

--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -45,31 +45,37 @@ export const Authentication = ({ user }) => {
     }
 
     return (
-        <section>
-            <h1>Economy tracker</h1>
-            <form>
-                <legend>{isSignUpActive ? "Sign Up" : "Sign In"}</legend>
-                <fieldset>
-                    <ul>
-                        <li>
-                            <label htmlFor='email'>Email</label>
-                            <input type='text' id='email' placeholder='email' onChange={handleEmailChange} className='border border-solid'></input>
-                        </li>
-                        <li>
-                            <label htmlFor='password'>Password</label>
-                            <input type='password' id='password' placeholder='password' onChange={handlePasswordChange} className='border border-solid'></input>
-                        </li>
-                    </ul>
+        <section className='flex flex-col gap-12 items-center justify-center h-screen'>
 
-                    {isSignUpActive ?
-                        <button type='button' onClick={handleSignUp} className='bg-blue-500 text-white'>Sign Up</button>
-                        :
-                        <button type='button' onClick={handleSignIn} className='bg-blue-500 text-white'>Sign In</button>
-                    }
+            <h1 className='text-4xl text-center tracking-wider underline'>Economy tracker</h1>
 
-                </fieldset>
-                <button type='button' onClick={handleMethodChange}>{isSignUpActive ? "Login" : "Create an account"}</button>
-            </form>
+            <div className='flex flex-col gap-7 px-14 py-14 shadow-blue-950 shadow-sm'>
+
+                <form className='flex flex-col gap-8'>
+                    <legend className='text-xl text-center font-medium tracking-wider'>{isSignUpActive ? "Sign Up" : "Sign In"}</legend>
+                    <fieldset className='flex flex-col gap-6 items-center'>
+
+                        <div className='flex flex-col gap-5 items-end'>
+                            <div className='flex flex-row gap-4 items-center'>
+                                <label htmlFor='email'>Email</label>
+                                <input type='email' id='email' placeholder='email@email.com' onChange={handleEmailChange} className='border border-solid p-1 rounded'></input>
+                            </div>
+                            <div className='flex flex-row gap-4 items-center'>
+                                <label htmlFor='password'>Password</label>
+                                <input type='password' id='password' placeholder='password' onChange={handlePasswordChange} className='border border-solid p-1 rounded'></input>
+                            </div>
+                        </div>
+
+                        {isSignUpActive ?
+                            <button type='button' onClick={handleSignUp} className='bg-yellow-400 hover:bg-yellow-500 rounded w-1/2'>NEW USER</button>
+                            :
+                            <button type='button' onClick={handleSignIn} className='bg-blue-500 text-white hover:bg-blue-600 rounded w-1/2'>SIGN IN</button>
+                        }
+
+                    </fieldset>
+                    <button type='button' onClick={handleMethodChange} className='underline text-blue-500 hover:text-blue-700'>{isSignUpActive ? "Login" : "Create an account"}</button>
+                </form>
+            </div>
         </section>
     )
 }
