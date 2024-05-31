eb1727c7dd68c620bba0e41de46d91cbc58d619d
Kristiana <kristianausacka@gmail.com>
2024-05-31 19:11:40 +0200

    Remove google auth option

--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -7,38 +7,24 @@ export const Authentication = ({ user }) => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    // Ui component conditional rendering:
     const [isSignUpActive, setIsSignUpActive] = useState(false);
 
-    const handleMethodChange = () => {
-        setIsSignUpActive(!isSignUpActive)
-    }
-
-    const handleSignUp = () => {
-        if (!email || !password) return;
-        createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
-            const user = userCredential.user;
-            console.log(user);
-        }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorCode, errorMessage)
-        })
-    }
-
-    const handleSignIn = () => {
-        if (!email || !password) return;
-        signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
-            const user = userCredential.user;
-            console.log(user);
-        }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorCode, errorMessage)
-        })
-    }
-
-    const handleEmailChange = (event) => setEmail(event.target.value);
-    const handlePasswordChange = (event) => setPassword(event.target.value);
+    const signUp = async () => {
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+    const signIn = async () => {
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
     if (user) {
         return <Navigate to="/"></Navigate>
@@ -58,22 +44,25 @@ export const Authentication = ({ user }) => {
                         <div className='flex flex-col gap-5 items-end'>
                             <div className='flex flex-row gap-4 items-center'>
                                 <label htmlFor='email'>Email</label>
-                                <input type='email' id='email' placeholder='email@email.com' onChange={handleEmailChange} className='border border-solid p-1 rounded'></input>
+                                <input type='email' id='email' placeholder='email@email.com' onChange={(e) => setEmail(e.target.value)} className='border border-solid p-1 rounded'></input>
                             </div>
                             <div className='flex flex-row gap-4 items-center'>
                                 <label htmlFor='password'>Password</label>
-                                <input type='password' id='password' placeholder='password' onChange={handlePasswordChange} className='border border-solid p-1 rounded'></input>
+                                <input type='password' id='password' placeholder='password' onChange={(e) => setPassword(e.target.value)} className='border border-solid p-1 rounded'></input>
                             </div>
                         </div>
 
                         {isSignUpActive ?
-                            <button type='button' onClick={handleSignUp} className='bg-yellow-400 hover:bg-yellow-500 rounded w-1/2'>NEW USER</button>
-                            :
-                            <button type='button' onClick={handleSignIn} className='bg-blue-500 text-white hover:bg-blue-600 rounded w-1/2'>SIGN IN</button>
+                            <button type='button' onClick={signUp} className='bg-yellow-400 hover:bg-yellow-500 rounded w-1/2'>NEW USER</button>
+                            : (
+
+                                <button type='button' onClick={signIn} className='bg-blue-500 text-white hover:bg-blue-600 rounded w-1/2'>SIGN IN</button>
+
+                            )
                         }
 
                     </fieldset>
-                    <button type='button' onClick={handleMethodChange} className='underline text-blue-500 hover:text-blue-700'>{isSignUpActive ? "Login" : "Create an account"}</button>
+                    <button type='button' onClick={() => setIsSignUpActive(!isSignUpActive)} className='underline text-blue-500 hover:text-blue-700'>{isSignUpActive ? "Login" : "Create an account"}</button>
                 </form>
             </div>
         </section>
