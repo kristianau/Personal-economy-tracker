4512bbdb1d19e12cbc9b5a125b89e35a25b04dda
Kristiana <kristianausacka@gmail.com>
2024-05-31 19:08:58 +0200

    Add .env file to hide firebase configuration variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyB-GX8CXI6BYgq0c6oYePWJqEr6vPL2fis",
-    authDomain: "economy-tracker-a9b7f.firebaseapp.com",
-    projectId: "economy-tracker-a9b7f",
-    storageBucket: "economy-tracker-a9b7f.appspot.com",
-    messagingSenderId: "629210728447",
-    appId: "1:629210728447:web:4ecaa4ccca0f34150ec9ca"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
@@ -19,3 +19,4 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
