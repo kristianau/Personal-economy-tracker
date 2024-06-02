ee2a5d45636407eb6fa7afcccc55a510015d9540
Kristiana <kristianausacka@gmail.com>
2024-06-02 13:03:25 +0200

    Savings dropdown in expenses page fetches information from db

--- src/components/InputElements/TakeFromSavingsDropdown.jsx
+++ src/components/InputElements/TakeFromSavingsDropdown.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Dropdown from "../BaseElements/Dropdown";
 import { Controller, useFormContext } from "react-hook-form";
 import ErrorMessage from "../Messages/ErrorMessage";
+import { db } from "../../firebase";
+import { getDocs, collection } from "firebase/firestore";
 
 export default function TakeFromSavingsDropdown({ label, id, placeholder }) {
 
     const { control } = useFormContext();
+
+    const [options, setOptions] = useState([]);
+
+    let multiple = false;
+    const collectionRef = collection(db, "savingsGoal");
+
+    useEffect(() => {
+        const getOptions = async () => {
+            // Read the data
+            try {
+                const data = await getDocs(collectionRef);
+                const filteredData = data.docs.map((doc) => ({ ...doc.data(), docId: doc.id }))
+                setOptions(filteredData)
+            } catch (err) {
+                console.error(err)
+            }
+            // Set the savings options list
+        }
+        getOptions();
+    }, [collectionRef])
 
     return (
         <div className="flex flex-col w-full gap-2">
@@ -26,7 +48,7 @@ export default function TakeFromSavingsDropdown({ label, id, placeholder }) {
                             <ErrorMessage error={error} />
                         </div>
                         <div className="flex flex-row gap-2 items-center">
-                            <Dropdown placeholder={placeholder} field={field} id={id} />
+                            <Dropdown placeholder={placeholder} field={field} options={options} multiple={multiple} id={id} />
                         </div>
                     </div >
                 )
